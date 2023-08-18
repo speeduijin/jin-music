b45a731fc1ab0ace50592520edf745f6ca163b0a
Jeong Euijin <speeduijin91@gmail.com>
2023-08-18 22:12:22 +0900

    feat: Transition project to API server

--- client/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['airbnb', 'airbnb-typescript', 'prettier'],
-  ignorePatterns: ['.eslintrc.cjs', 'build'],
-};
